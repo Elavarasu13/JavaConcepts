5d13700f931839853fe8c5f07492f114cd53f1a4
Elavarasu13 <elavarasu.subramaniam@gmail.com>
2024-01-18 22:41:37 +0530

    Sort

---
+++ Sort.java
@@ -0,0 +1,16 @@
+package ArrayTasks;
+
+import java.util.Scanner;
+
+public class Sort {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter the Student Name:");
+		String s = sc.nextLine();
+
+	}
+
+}
